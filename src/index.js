531bc2ac4f3be15044f583d31085e582586e7025
hdoupe <henrymdoupe@gmail.com>
2019-01-29 21:57:00 -0500

    Refactor into schema and param components

--- src/index.js
+++ src/index.js
@@ -2,40 +2,111 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class ParamForm extends React.Component {
+class Value extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
+  handleChange(e) {
+    this.props.handleValueChange(e.target.value)
   }
 
   render() {
+    const value = this.props.value;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Schema name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+      <fieldset>
+        <label> Value:
+          <input value={value} onChange={this.handleChange}/>
         </label>
-        <input type="submit" value="Submit" />
-      </form>
+      </fieldset>
     );
   }
 }
 
+class Param extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      value: '',
+    };
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleValueChange = this.handleValueChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    this.setState({name: e.target.value});
+    this.props.handleParamsChange({[this.state.name]: this.state.value});
+  }
+
+  handleValueChange(value) {
+    this.setState({value});
+    this.props.handleParamsChange({[this.state.name]: this.state.value});
+  }
+
+  render() {
+    const name = this.state.schemaName;
+    const value = this.state.value;
+    return (
+      <fieldset>
+        <label>
+          Parameter Name:
+          <input type="text" name="name" value={name} onChange={this.handleNameChange} />
+        </label>
+        <Value handleValueChange={this.handleValueChange} value={value}/>
+      </fieldset>
+    );
+  }
+}
+
+class Schema extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      schemaName: "",
+      params: {},
+    };
+    this.handleSchemaNameChange = this.handleSchemaNameChange.bind(this);
+    this.handleParamsChange = this.handleParamsChange.bind(this);
+  }
+
+  handleSchemaNameChange(e) {
+    this.setState({schemaName: e.target.value});
+  }
+
+  handleParamsChange(params) {
+    var newparams = Object.assign(this.state.params, params);
+    this.setState({params: newparams});
+  }
+
+  render() {
+    const schemaName = this.state.schemaName;
+    const paramstring = JSON.stringify(this.state.params);
+    return (
+      <fieldset>
+        <h1>paramTools UI</h1>
+        <label>
+        Schema Name:
+        <input
+          type="text"
+          name="name"
+          value={schemaName}
+          onChange={this.handleSchemaNameChange} />
+        </label>
+        <p>{paramstring}</p>
+        <Param handleParamsChange={this.handleParamsChange}/>
+        <Param handleParamsChange={this.handleParamsChange}/>
+      </fieldset>
+    );
+  }
+}
+
+
 // ========================================
 
 ReactDOM.render(
-  <ParamForm />,
+  <Schema />,
   document.getElementById('root')
 );
